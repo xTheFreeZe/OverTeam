5c8bddc1fd31d852a21fa532bc561d218333cf0e
Marwin <marwin.eder@gmx.de>
2023-02-05 17:46:07 +0100

    Old file system bruh

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,45 +4,20 @@ const {Bot_Token, Client_ID} = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
+const ScheduleFiles = fs.readdirSync('./commands/schedule').filter(file => file.endsWith('.js'));
+const TestFiles = fs.readdirSync('./commands/testing').filter(file => file.endsWith('.js'))
 
-fs.readdir('./commands/', (err, folders) => {
+for (const file of ScheduleFiles) {
+  const command = require(`./commands/schedule/${file}`);
+  commands.push(command.data.toJSON());
+  console.log('Recieved Schedule files');
+}
 
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  for (const folder of folders) async () => {
-
-    console.log('Folder name:', folder);
-
-    console.log('Looping further...');
-
-    fs.readdir(`./commands/${folder}`, async (err, file) => {
-
-      console.log(file);
-
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      file.forEach((f) => {
-
-        console.log('File name:', f);
-
-        const command = require(`./commands/${folder}/${f}`);
-        const data = command.data.toJSON();
-
-        commands.push(data);
-
-        console.log('Commands:', commands);
-      });
-
-    });
-  };
-
-});
+for (const file of TestFiles) {
+  const command = require(`./commands/testing/${file}`);
+  commands.push(command.data.toJSON());
+  console.log('Recieved Testing files');
+}
 
 const rest = new REST({version: '9'}).setToken(Bot_Token);
 
@@ -51,13 +26,14 @@ const rest = new REST({version: '9'}).setToken(Bot_Token);
 
     console.log('Started refreshing application (/) commands.');
 
+    console.log(commands);
+
     await rest.put(
         Routes.applicationCommands(Client_ID),
         {body: commands},
     );
 
     console.log('Successfully reloaded application (/) commands.');
-
   } catch (error) {
     console.error(error);
   }
