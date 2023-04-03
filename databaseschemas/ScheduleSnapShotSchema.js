1cc0433ffa56f0992bfac2551023b5e6d95b39d8
Marwin <marwin.eder@gmx.de>
2023-04-04 00:27:53 +0200

    New schema

---
+++ databaseschemas/ScheduleSnapShotSchema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const requiredString = {
+  type: String,
+  required: true,
+};
+
+const nonRequiredString = {
+  type: String,
+  required: false,
+};
+
+const requiredNumber = {
+  type: Number,
+  required: true,
+}
+
+const ScheduleSnapShotSchema = new mongoose.Schema({
+  _id: requiredNumber,
+  name: requiredString,
+  creationChannel: requiredString,
+  channelID: requiredNumber,
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  description: requiredString,
+  scheduleCreator: requiredString,
+  scheduleCreatorID: requiredNumber,
+  users: {
+    userOne: nonRequiredString,
+    userTwo: nonRequiredString,
+    userThree: nonRequiredString,
+    userFour: nonRequiredString,
+    userFive: nonRequiredString,
+    userSix: nonRequiredString,
+    userSeven: nonRequiredString,
+    userEight: nonRequiredString,
+    userNine: nonRequiredString,
+    userTen: nonRequiredString,
+  },
+});
+
+module.exports = mongoose.model('SchedulePreset', ScheduleSnapShotSchema);
+
+console.log('Exported ScheduleSnapShotSchema.js');
