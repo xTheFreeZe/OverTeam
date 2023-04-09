a3138128418cf3b7a7e8ebc219466468dcc0cfa2
Marwin <marwin.eder@gmx.de>
2023-04-09 22:51:30 +0200

    redis

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,8 +1,20 @@
+const GetScheduleData = require('../functions/schedules/standert/GetScheduleData.js');
+
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction) {
 
     if (!interaction.isButton()) return;
+
+    // Get the identifier from the custom ID. This will be used to get the data from the database.
+    const identifier = interaction.customId.substring(0, 8);
+
+    console.log('Identifier:', identifier);
+
+    // Get the data from the database.
+    const data = await GetScheduleData(identifier);
+
+    console.log('Data:', data);
     
     interaction.reply('Button pressed. Custom ID: ' + interaction.customId);
 
